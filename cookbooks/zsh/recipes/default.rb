4130a9a66d153d7bd0a0d31fd1e19160d53f9f6d
aibou <aibou7251@gmail.com>
2013-08-07 17:00:23 +0900

    modified for ca mbp

--- cookbooks/zsh/recipes/default.rb
+++ cookbooks/zsh/recipes/default.rb
@@ -14,6 +14,6 @@ end
 template File.expand_path "~/.zshrc" do
   source "zshrc.erb"
   mode 0664
-  owner node[:file_mod][:user]
-  group node[:file_mod][:group]
+  owner node[:zsh][:file][:user]
+  group node[:zsh][:file][:group]
 end
