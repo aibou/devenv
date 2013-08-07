aed8f9709b4e479e6456a5e447b60479be3d0e1c
aibou <aibou7251@gmail.com>
2013-08-07 19:37:49 +0900

    add rbenv cookbook (installeb by homebrew)

---
+++ cookbooks/rbenv/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: rbenv
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package %w{rbenv ruby-build} do 
+  action :install
+end
