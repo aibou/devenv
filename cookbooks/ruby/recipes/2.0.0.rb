94f1a1b4fefdf2a42cc5824358bd9ae198b584a0
aibou <aibou7251@gmail.com>
2013-08-20 21:46:50 +0900

    tmp

--- cookbooks/ruby/recipes/2.0.0.rb
+++ cookbooks/ruby/recipes/2.0.0.rb
@@ -1,14 +1,8 @@
 #
-# Cookbook Name:: rbenv
+# Cookbook Name:: ruby
 # Recipe:: default
 #
 # Copyright 2013, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
 #
-
-%w{rbenv ruby-build}.each do |pkg|
-  package pkg do
-    action :install
-  end
-end
